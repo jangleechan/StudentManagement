a42e17365dee95639204453061224d97e2fe9826
Leechan <Jangleechan@outlook.com>
2023-03-29 19:53:51 +0800

    Revert "添加项目文件。"
    This reverts commit d86355ae530f7f507a70763576d81469e9c5e607.

--- WebMvc/Models/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace StudentManagement.Models
-{
-    public class ApplicationUser:IdentityUser
-    {
-        public string City { get; set; }
-    }
-}
