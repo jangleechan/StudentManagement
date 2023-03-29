657e2aeff9e5d704dfd6ff37e86671d8b233b605
Leechan <Jangleechan@outlook.com>
2023-03-29 19:53:51 +0800

    add EditRoles ModuleRevert "添加项目文件。"
    This reverts commit d86355ae530f7f507a70763576d81469e9c5e607.Revert "添加项目文件。"
    
    This reverts commit d86355ae530f7f507a70763576d81469e9c5e607.

--- WebMvc/Controllers/AdminController.cs
+++ WebMvc/Controllers/AdminController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using StudentManagement.Models;
 using StudentManagement.ViewModels;
 
 namespace StudentManagement.Controllers
@@ -8,9 +9,12 @@ namespace StudentManagement.Controllers
     {
         private RoleManager<IdentityRole> roleManager;
 
-        public AdminController(RoleManager<IdentityRole> roleMnager)
+        public UserManager<ApplicationUser> UserManager { get; }
+
+        public AdminController(RoleManager<IdentityRole> roleMnager, UserManager<ApplicationUser> userManager)
         {
             this.roleManager = roleMnager;
+            this.UserManager = userManager;
         }
 
         // Get  Post
@@ -50,6 +54,60 @@ namespace StudentManagement.Controllers
         {
             var roles = roleManager.Roles;
             return View(roles);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditRoles(string id)
+        {
+            var role = await roleManager.FindByIdAsync(id);
+
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"角色ID为{id}的信息不存在，请重试。";
+                return View("NotFound");
+            }
+
+            var model = new EditRoleViewModel { Id = id, RoleName = role.Name };
+            var users = UserManager.Users.ToList();
+            foreach (var user in users)
+            {
+                // 如果用户拥有此角色，请将用户名添加到
+                // EditRoleViewModel 模型中的Users属性中
+                if(await UserManager.IsInRoleAsync(user, role.Name))
+                {
+                    model.Users.Add(user.UserName);
+                }
+            }
+            //然后江对象传递给试图显示到客户端
+            return View(model);
+        }
+
+        public async Task<IActionResult> EditRoles(EditRoleViewModel model)
+        {
+            var role = await roleManager.FindByIdAsync(model.Id);
+
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"角色ID为{model.Id}的信息不存在，请重试。";
+                return View("NotFound");
+            }
+            else
+            {
+                role.Name = model.RoleName;
+                var result = await roleManager.UpdateAsync(role);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("ListRoles");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+
+            return View(model);
+
         }
     }
 }
---
+++ WebMvc/ViewModels/EditRoleViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Cors;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentManagement.ViewModels
+{
+    public class EditRoleViewModel
+    {
+        [Display(Name = "角色ID")]
+        public string Id { get; set; }
+        [Required]
+        [Display(Name ="角色名称")]
+        public string RoleName { get; set; }   
+        
+        public List<string> Users { get; set; }
+
+    }
+}
